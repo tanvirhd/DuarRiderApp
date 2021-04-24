fd4e330b43d9b7a58ec7e8687d84d6d48016991b
Tanvir Hossain <tanvir.taagidtech@gmail.com>
2021-04-25 00:18:26 +0600

    Initial commit

---
+++ app/src/main/java/com/duarbd/duarriderapp/presenter/ActivityHome.java
@@ -0,0 +1,16 @@
+package com.duarbd.duarriderapp.presenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.duarbd.duarriderapp.R;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
